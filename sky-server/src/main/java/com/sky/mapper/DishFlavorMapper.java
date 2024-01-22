4c17dc46502dc32c38a22e0a3f81c4515527a0c4
haishuozhang <13996102+haishuozhang@user.noreply.gitee.com>
2024-01-22 15:55:48 +0800

    完成修改菜品模块

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -3,6 +3,7 @@ package com.sky.mapper;
 import com.sky.entity.DishFlavor;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -15,6 +16,14 @@ public interface DishFlavorMapper {
      * 根据id删除口味信息
      * @param id
      */
-    @Delete("delete from dish_flavor where id = #{id}")
+    @Delete("delete from dish_flavor where dish_id = #{id}")
     void deleteById(Long id);
+
+    /**
+     * 根据id查询口味信息
+     * @param id
+     * @return
+     */
+    @Select("select * from dish_flavor where dish_id = #{id}")
+    List<DishFlavor> getById(Long id);
 }
--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -109,4 +109,52 @@ public class DishServiceImpl implements DishService {
             dishFlavorMapper.deleteById(id);
         }
     }
+
+    /**
+     * 根据id查询菜品
+     * @param id
+     * @return
+     */
+    @Override
+    public DishVO getByIdWithFlavor(Long id) {
+
+        // 根据id查询菜品信息
+        Dish dish = dishMapper.getById(id);
+
+        // 根据id查询口味信息
+        List<DishFlavor> dishFlavors = dishFlavorMapper.getById(id);
+
+        // 将查询到的数据封装到VO返回
+        DishVO dishVO = new DishVO();
+        BeanUtils.copyProperties(dish, dishVO);
+        dishVO.setFlavors(dishFlavors);
+        return dishVO;
+    }
+
+    /**
+     * 修改菜品
+     * @param dishDTO
+     */
+    @Override
+    public void updateWithFlavor(DishDTO dishDTO) {
+
+        Dish dish = new Dish();
+        BeanUtils.copyProperties(dishDTO, dish);
+        // 修改菜品表基本信息
+        dishMapper.update(dish);
+
+        //删除原有的口味数据
+        dishFlavorMapper.deleteById(dishDTO.getId());
+
+        //重新插入口味数据
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+        if (flavors != null && !flavors.isEmpty()) {
+
+            flavors.forEach(flavor -> {
+                flavor.setDishId(dishDTO.getId());
+            });
+
+            dishFlavorMapper.insert(flavors);
+        }
+    }
 }
